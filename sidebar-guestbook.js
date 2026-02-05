05aac414f68e54768bda122ec000680c58dbfa62
lavirox <lavanya23x@gmail.com>
2026-02-05 02:45:23 -0800

    still trying to fix guestbook, another fix try

--- sidebar-guestbook.js
+++ sidebar-guestbook.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
